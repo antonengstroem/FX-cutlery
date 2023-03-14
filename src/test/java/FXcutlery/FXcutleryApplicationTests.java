7080150326d7d80ef6799cbe4115bdf5eb18e386
antonengstroem <anton.engstrom95@gmail.com>
2023-03-14 21:30:35 +0100

    setup project

---
+++ src/test/java/FXcutlery/FXcutleryApplicationTests.java
@@ -0,0 +1,13 @@
+package FXcutlery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FXcutleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
